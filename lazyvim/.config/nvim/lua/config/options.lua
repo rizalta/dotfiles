9c0083e7c95edab444120714d571b7cec3ba2efa
rizalta <rizalta00@gmail.com>
2025-06-23 19:51:55 +0530

    Added lazyvim

---
+++ lazyvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,7 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.opt.tabstop = 2 -- A tab character looks like 2 spaces
+vim.opt.shiftwidth = 2 -- Indent by 2 spaces
+vim.opt.softtabstop = 2 -- A tab press inserts 2 spaces
+vim.opt.expandtab = true -- Use spaces instead of tabs
---
+++ lazyvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,8 @@
+return {
+	"neovim/nvim-lspconfig",
+	opts = {
+		servers = {
+			cssls = { settings = { css = { lint = { unknownAtRules = "ignore" } } } },
+		},
+	},
+}
