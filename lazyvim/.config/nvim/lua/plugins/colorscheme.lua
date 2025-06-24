81ed5cbea6a160efd72567240633c42fd0a9253b
rizalta <rizalta00@gmail.com>
2025-06-24 18:51:14 +0530

    changes to lazyvim

---
+++ lazyvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		opts = {
+			flavor = "frappe",
+		},
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin",
+		},
+	},
+}
