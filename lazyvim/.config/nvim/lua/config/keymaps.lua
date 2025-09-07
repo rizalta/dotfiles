2b6644e2b658a21feee8564e470fb4aac08f84ee
rizalta <rizalta00@gmail.com>
2025-09-07 17:06:30 +0530

    removed arrowkeys from nvim

--- lazyvim/.config/nvim/lua/config/keymaps.lua
+++ lazyvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+for _, mode in ipairs({ "n", "i", "v" }) do
+	vim.keymap.set(mode, "<Up>", "<Nop>", { noremap = true, silent = true })
+	vim.keymap.set(mode, "<Down>", "<Nop>", { noremap = true, silent = true })
+	vim.keymap.set(mode, "<Left>", "<Nop>", { noremap = true, silent = true })
+	vim.keymap.set(mode, "<Right>", "<Nop>", { noremap = true, silent = true })
+end
